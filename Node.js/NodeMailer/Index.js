a1e7b602f142cd23159c585a92782ee45656e208
KhanzaidGithub <zaidscestudent@gmail.com>
2024-11-25 02:19:46 -0800

    nodemailer it worked

--- Node.js/NodeMailer/Index.js
+++ Node.js/NodeMailer/Index.js
@@ -1,10 +1,34 @@
 var http = require('http');
+var nodemailer = require('nodemailer');
 
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': "text/html" })
-    console.log('Hellwo world');
-    res.write('Hello world')
-    res.end();
+    var transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user:"myemail@gmail.com",
+            pass: "it's private",
+        }
+    })
 
+    let mailOption = {
+        from: 'zaidwork76@gmail.com',
+        to: "zaidscestudent@gmail.com",
+        subject: "Hi This is checking Nodemailer",
+        text: "Hi This is checking Nodemailer tester"
+    }
+    transporter.sendMail(mailOption, function (error, info) {
+        if (error)
+            throw error
+
+        alert(infoj)
+
+    })
 }).listen(8080)
+
+// var http = require('http');
+
+// http.createServer(function (req, res){
+//     res.write('Hello world')
+//     res.end()
+// }).listen(8080)
