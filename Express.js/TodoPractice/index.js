7d121150fe0b206871d9f49106d74d56099c21de
KhanzaidGithub <zaidscestudent@gmail.com>
2024-12-03 08:11:48 -0800

    Express Routing System

--- Express.js/TodoPractice/index.js
+++ Express.js/TodoPractice/index.js
@@ -1,6 +1,44 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
+
+
+const todos = [
+  {
+    title: "Todo 1",
+    desc: "This is my first Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 2",
+    desc: "This is my second Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 3",
+    desc: "This is my third Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 4",
+    desc: "This is my fourth Todo",
+    completed: true,
+  },
+
+  {
+    title: "Todo 5",
+    desc: "This is my fifth Todo",
+    completed: true,
+  },
+];
+
+
+app.get('/todo',(req,res)=>{
+  res.status(200).json(todos)
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
