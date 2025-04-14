2559d329d1b8bda1b67afd24eb58ba17f1fd2f3c
KhanzaidGithub <zaidscestudent@gmail.com>
2025-04-13 21:37:42 -0700

    .

---
+++ MySql/Introduction.js
@@ -0,0 +1,13 @@
+// DQL(Data Query Language) : Used to retrieve data from databases. (SELECT)
+// DDL(Data Definition Language) : Used to create, alter, and delete database objects
+// like tables, indexes, etc. (CREATE, DROP, ALTER, RENAME, TRUNCATE)
+// DML(Data Manipulation Language): Used to modify the database. (INSERT,
+// UPDATE, DELETE)
+// DCL(Data Control Language): Used to grant & revoke permissions. (GRANT,
+// REVOKE)
+// TCL(Transaction Control Language): Used to manage transactions. (COMMIT,
+// ROLLBACK, START TRANSACTIONS, SAVEPOINT)
+
+// SQL v / s MySQL
+//  SQL is a language used to perform CRUD operations in Relational DB, while MySQL is a
+//  RDBMSthat uses SQL.
---
+++ MySql/Joins.js
@@ -0,0 +1,12 @@
+// Joins in MySql
+
+
+// JOINS
+//  In a DBMS, a join is an operation that combines rows from two or more tables based on a
+//  related column between them.
+//  Joins are used to retrieve data from multiple tables by linking them together using a common
+
+// Inner Join
+// Outer Join
+// Cross Join
+// Self Join
